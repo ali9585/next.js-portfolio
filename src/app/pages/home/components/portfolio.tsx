dc86ea584a643b695f113accb426465eb85687fb
ali9585 <alikhan9585497@gmail.com>
2023-11-24 00:51:56 +0500

    portfolio section added

--- src/app/pages/home/components/portfolio.tsx
+++ src/app/pages/home/components/portfolio.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { SectionBadge } from "@/app/utils/sectionBadge";
 import { LiaGripVerticalSolid } from "react-icons/lia";
 
@@ -6,6 +7,19 @@ const Portfolio: React.FC = (): JSX.Element => {
         <>
             <div className="portfolio">
                 <SectionBadge title='Portfolio' icon={<LiaGripVerticalSolid className='icon' size={14} color='#fff' />} />
+                <h1 className="section-title">
+                    Featured <span>Projects</span>
+                </h1>
+                <div className="projects">
+                    <div className="item">
+                        <Link href=''>
+                            <div className="image">
+                                <img src="https://wpriverthemes.com/HTML/drake/assets/images/portfolio3.jpg" alt="" />
+                            </div>
+                        </Link>
+                        <Link href='' className="title">Bureau - Architecture Studio Website</Link>
+                    </div>
+                </div>
             </div>
         </>
     );
