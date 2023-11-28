ef54fed094e74ab9a59dc59625b592c039c48995
ali9585 <alikhan9585497@gmail.com>
2023-11-28 10:48:18 +0500

    pricing section completed

--- src/app/pages/home/components/pricing.tsx
+++ src/app/pages/home/components/pricing.tsx
@@ -9,20 +9,61 @@ const Pricing: React.FC = (): JSX.Element => {
                 <h1 className="section-title">
                     My <span>Pricing</span>
                 </h1>
-                <div className="grid grid-cols-2 gap-[10px]">
+                <div className="grid md:grid-cols-2 sm:grid-cols-1 gap-[10px]">
                     <div className="card">
                         <div className="status flex justify-between">
                             <h4>Basic</h4>
                             <p>Have design ready to build? <br /> or small budget</p>
                         </div>
+                        <div className="price">
+                            <h1>
+                                $49
+                                <span>/ hours</span>
+                            </h1>
+                        </div>
+                        <div className="features">
+                            <ul>
+                                <li>Need your wireframe</li>
+                                <li>Design with Figma, Framer</li>
+                                <li>Implement with Webflow, React, WordPress, Laravel/PHP</li>
+                                <li>Remote/Online</li>
+                                <li>Work in business days, no weekend.</li>
+                                <li>Support 6 months</li>
+                            </ul>
+                        </div>
+                        <button className="theme-btn">Pick This Package</button>
                     </div>
                     <div className="card">
                         <div className="status flex justify-between">
                             <h4>Basic</h4>
                             <p>Have design ready to build? <br /> or small budget</p>
                         </div>
+                        <div className="price">
+                            <h1>
+                                $49
+                                <span>/ hours</span>
+                            </h1>
+                        </div>
+                        <div className="features">
+                            <ul>
+                                <li>Need your wireframe</li>
+                                <li>Design with Figma, Framer</li>
+                                <li>Implement with Webflow, React, WordPress, Laravel/PHP</li>
+                                <li>Remote/Online</li>
+                                <li>Work in business days, no weekend.</li>
+                                <li>Support 6 months</li>
+                                <li>Remote/Online</li>
+                                <li>Work in business days, no weekend.</li>
+                                <li>Support 6 months</li>
+                            </ul>
+                        </div>
+                        <button className="theme-btn">Pick This Package</button>
                     </div>
                 </div>
+                <p className="info">
+                    Don't find any package match with your plan!<br />
+                    Want to setup a new tailor-made package for only you?. <span>Contact Us</span>
+                </p>
             </div>
         </>
     );
